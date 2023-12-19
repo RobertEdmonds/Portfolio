fe8e180dccb9c5366888523f31928e5fce55fd3d
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-12-19 17:41:58 -0500

    Add writing to project

--- src/Projects.js
+++ src/Projects.js
@@ -40,6 +40,14 @@ export default function Projects(){
                         <p className='Project-writing'>
                             This application is password protected for keeping inventory for a sand company. Aqua Prop is able to keep track of the sand showing up on site and the sand used on site. Also there was a certain equation to keep track of moisture. If there was some delivered bad sand they could also keep inventory of amount. Aqua Prop had the ability to export the data to an Excel sheet. For the companies hiring Aqua Prop, they were able to keep track of the their sites.
                         </p>
+                        <ul className='skill-list'>
+                            <li className='Project-writing'>Contributed ideas and suggestions in team meetings, actively utilizing React, JavaScript algorithms, Bootstrap, Material UI, Ruby on Rails, and serialization, and consistently delivered updates on deadlines, designs, and enhancements.</li>
+                            <br/>
+                            <li className='Project-writing'>Enhanced software performance by updating the way data is delivered from the Ruby on Rails backend and optimizing algorithms on the React frontend, resulting in a 40% improvement in data processing and UI load speeds. Integrated these solutions seamlessly into upcoming releases to ensure sustained optimization.</li>
+                            <br/>
+                            <li className='Project-writing'>Spearheaded the transition from the Excel program to a new website, leveraging a React frontend with Material UI, Bootstrap, and CSS for design, supported by a robust Ruby on Rails backend and a PostgreSQL database. This initiative resulted in improved training methods and an exceptional 80% reduction in errors.</li>
+                            <br/>
+                        </ul>
                     </div>
                     <h3 className='Project-title'>Programs</h3>
                     <ul className="skill-list">
@@ -54,6 +62,7 @@ export default function Projects(){
                         <li className="Project-item">CSS</li>
                         <li className="Project-item">React</li>
                         <li className="Project-item">React Router Dom</li>
+                        <li className="Project-item">SCRUM</li>
                     </ul>
                     <h3 className='Project-title'>Photo <div style={{display: '-webkit-box'}}><button
                         className="leftArrowButton"
